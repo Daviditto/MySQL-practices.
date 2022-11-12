071c3ffa242840ddb4525ab05f3b85a1f8fa986b
David (Hui) Liu <93046901+Daviditto@users.noreply.github.com>
2022-11-12 09:31:10 -0700

    Add files via upload

---
+++ MySQL practice question no.1.sql
@@ -0,0 +1,22 @@
+select * from emp;
+select * from dept;
+
+select 
+dept.dname, a.pct_over_2500, a.numbers_of_employees
+from
+(select 
+sum(case when t.sal is null then 0 else 1 end)/count(emp.sal) as pct_over_2500,
+emp.deptno, 
+count(emp.ename) as numbers_of_employees
+from emp 
+left join
+(select  deptno, ename, sal
+from emp
+where sal > 2500) as t
+on emp.ename = t.ename
+group by 2) a inner join dept
+on a.deptno=dept.deptno;
+
+
+
+
