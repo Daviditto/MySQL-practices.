9254d30db376ccea29009415dca3c2504973cb13
David (Hui) Liu <93046901+Daviditto@users.noreply.github.com>
2022-11-12 09:37:31 -0700

    Add files via upload
    Given a employees and departments table, select the departments and rank their according to the percentage of their employees making over 100K in salary.

--- MySQL practice question no.1.sql
+++ MySQL practice question no.1.sql
@@ -2,7 +2,7 @@ select * from emp;
 select * from dept;
 
 select 
-dept.dname, a.pct_over_2500, a.numbers_of_employees
+dept.dname, a.numbers_of_employees ,a.pct_over_2500
 from
 (select 
 sum(case when t.sal is null then 0 else 1 end)/count(emp.sal) as pct_over_2500,
@@ -15,8 +15,8 @@ from emp
 where sal > 2500) as t
 on emp.ename = t.ename
 group by 2) a inner join dept
-on a.deptno=dept.deptno;
-
+on a.deptno=dept.deptno
+order by 3 desc;
 
 
 
